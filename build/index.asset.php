b2a604d7ae36227b47e3bc19aea0a49dc827325c
unknown <jahirulislam41185@gmail.com>
2024-01-30 16:52:22 +0600

    add reachtext component

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '76d6929431e56fabd7a2');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3ec86d45c445b890fa1a');
